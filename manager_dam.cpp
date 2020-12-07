514f898a6ff9407970fba572d4a32bede2876bdd
ReneGliddon <37896877+ReneGliddon@users.noreply.github.com>
2020-12-07 15:00:48 +0900

    Add files via upload

---
+++ manager_dam.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include "manager_dam.h"
+
+using namespace std;
+
+int main(){
+
+    Manager jeff(60560, 5500, 2800, 0.14, 3100);
+
+    jeff.Display();
+
+    cout << "\nSalary Deduction: \tR " << jeff.calcDeductions() << endl;
+    cout << "Payable Tax: \t\tR " << jeff.calcTax() << endl;
+    cout << "Net Salary: \t\tR " << jeff.calcGrossPay() - jeff.calcTax() - jeff.calcDeductions() << endl;
+
+    return 0;
+}
